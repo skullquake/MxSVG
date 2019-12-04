ecef22e3689e0c68c02915c36ab18927a65c77b6
skullquake <ockert8080@gmail.com>
2019-12-03 22:05:38 -0800

    .

---
+++ src/MxSVG/widget/MxSVG.js
@@ -0,0 +1,110 @@
+require(
+	{
+		packages:[
+
+		]
+	},
+	[
+		"dojo/_base/declare",
+		"mxui/widget/_WidgetBase",
+		"dijit/_TemplatedMixin",
+		"mxui/dom",
+		"dojo/dom",
+		"dojo/dom-prop",
+		"dojo/dom-geometry",
+		"dojo/dom-class",
+		"dojo/dom-style",
+		"dojo/dom-construct",
+		"dojo/_base/array",
+		"dojo/_base/lang",
+		"dojo/text",
+		"dojo/html",
+		"dojo/_base/event",
+		"dojo/text!MxSVG/widget/template/MxSVG.html"
+	],
+	function(
+		declare,
+		_WidgetBase,
+		_TemplatedMixin,
+		dom,
+		dojoDom,
+		dojoProp,
+		dojoGeometry,
+		dojoClass,
+		dojoStyle,
+		dojoConstruct,
+		dojoArray,
+		lang,
+		dojoText,
+		dojoHtml,
+		dojoEvent,
+		widgetTemplate
+	){
+		return declare(
+			"MxSVG.widget.MxSVG",
+			[
+				_WidgetBase,
+				_TemplatedMixin
+			],
+			{
+				templateString:widgetTemplate,
+				widgetBase:null,
+				_handles:null,
+				_contextObj:null,
+				constructor: function () {
+					this._handles = [];
+				},
+				postCreate: function () {
+				},
+				update: function (obj, callback) {
+					this._contextObj = obj;
+					this._updateRendering(callback);
+				},
+				resize: function (box) {
+				},
+				uninitialize: function () {
+				},
+				destroy:function () {
+				},
+				_updateRendering: function (callback) {
+					if (this._contextObj !== null) {
+						if(!this.loaded){
+							this.loaded=true;
+							var fileurl="/file?guid="+this._contextObj.getGuid()+"&changeDate="+(new Date().getTime());
+						}else{
+						}
+						dojoStyle.set(this.domNode, "display", "block");
+					} else {
+						dojoStyle.set(this.domNode, "display", "none");
+					}
+
+					this._executeCallback(callback, "_updateRendering");
+				},
+				_execMf: function (mf, guid, cb) {
+					if (mf && guid) {
+						mx.ui.action(mf, {
+							params: {
+								applyto: "selection",
+								guids: [guid]
+							},
+							callback: lang.hitch(this, function (objs) {
+								if (cb && typeof cb === "function") {
+									cb(objs);
+								}
+							}),
+							error: function (error) {
+								console.debug(error.description);
+							}
+						}, this);
+					}
+				},
+				_executeCallback: function (cb, from) {
+					if (cb && typeof cb === "function") {
+						cb();
+					}
+				}
+			}
+		);
+	}
+);
+//require(["MxSVG/widget/MxSVG"]);
